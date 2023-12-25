71c72b7f5a8c78d1f2eb254d7c3c0c6fc1f07bd9
KaterynaKorn <kat.korneeva2011@gmail.com>
2023-12-25 15:38:34 +0200

    Task 13

--- Task_13/src/main/java/org/example/Main.java
+++ Task_13/src/main/java/org/example/Main.java
@@ -1,16 +1,25 @@
 package org.example;
 
 import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        Stream.generate(Generator::generateUser)
+        Map<String, List<User>> groupedGender = Stream.generate(Generator::generateUser)
                 .limit(20)
                 .filter(user -> user.getAge() > 18)
                 .sorted(Comparator.comparing(User::getName))
                 .filter(user -> !"".equals(user.getEmail()))
                 .sorted(Comparator.comparing(User::getGender))
-                .forEach(System.out::println);
+                .collect(Collectors.groupingBy(User::getGender));
+
+        groupedGender.forEach((gender, userList) -> {
+            System.out.println("Gender: " + gender);
+            userList.forEach(System.out::println);
+        });
     }
 }
